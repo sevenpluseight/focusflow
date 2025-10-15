648fd13f44bdbf5b71109369a6d6a28dcd429926
Ling2609 <TP080089@mail.apu.edu.my>
2025-10-15 18:01:43 +0800

    icon added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.focusflow"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName

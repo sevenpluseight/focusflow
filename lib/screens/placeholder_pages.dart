9e709ff6fbb341d2d731583297b9f6c7051cf6c8
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-10-27 17:18:21 +0800

    use theme's text color for improved visibility

--- lib/screens/placeholder_pages.dart
+++ lib/screens/placeholder_pages.dart
@@ -6,14 +6,16 @@ class PlaceholderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Use the theme's background color
     final backgroundColor = Theme.of(context).scaffoldBackgroundColor;
     return Container(
       color: backgroundColor,
       child: Center(
         child: Text(
           title,
-          style: const TextStyle(fontSize: 24, color: Colors.white), // Ensure text is visible
+          style: TextStyle(
+            fontSize: 24,
+            color: Theme.of(context).textTheme.bodyLarge?.color,
+          ),
         ),
       ),
     );

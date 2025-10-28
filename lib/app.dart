897b83312fc82147241f7398002ce4b58e80df9b
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-10-28 22:13:08 +0800

    refactor App widget to improve user role retrieval and error handling

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:focusflow/screens/auth/login_screen.dart';
-import 'package:focusflow/screens/main_navigation_controller.dart';
+import 'package:focusflow/screens/core/main_navigation_controller.dart';
 import 'package:focusflow/services/user_service.dart';
+import 'package:focusflow/models/models.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -22,11 +23,11 @@ class App extends StatelessWidget {
         }
 
         if (snapshot.hasData) {
-          return FutureBuilder<UserRole>(
+          return FutureBuilder<UserModel?>(
             key: ValueKey(snapshot.data!.uid),
-            future: UserService().getUserRole(snapshot.data!.uid),
-            builder: (context, roleSnapshot) {
-              if (roleSnapshot.connectionState == ConnectionState.waiting) {
+            future: UserService().getUser(snapshot.data!.uid),
+            builder: (context, userModelSnapshot) {
+              if (userModelSnapshot.connectionState == ConnectionState.waiting) {
                 return const Scaffold(
                   backgroundColor: Color(0xFF222428),
                   body: Center(
@@ -35,16 +36,26 @@ class App extends StatelessWidget {
                 );
               }
 
-              if (roleSnapshot.hasError) {
+              if (userModelSnapshot.hasError) {
                 // Handle error, maybe show an error screen
                 return const Scaffold(
                   body: Center(
-                    child: Text('Error fetching user role'),
+                    child: Text('Error fetching user data'),
                   ),
                 );
               }
 
-              final userRole = roleSnapshot.data ?? UserRole.user;
+              final UserModel? userModel = userModelSnapshot.data;
+              UserRole userRole = UserRole.user; // Default role
+
+              if (userModel != null) {
+                final roleString = userModel.role.toLowerCase();
+                userRole = UserRole.values.firstWhere(
+                  (e) => e.toString().split('.').last == roleString,
+                  orElse: () => UserRole.user,
+                );
+              }
+
               return MainNavigationController(
                 currentUserRole: userRole,
               );

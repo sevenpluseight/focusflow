36bd2e0e7f634b9f3d59a1bca52ce7c5041520e5
Ling2609 <TP080089@mail.apu.edu.my>
2025-11-13 17:31:49 +0800

    add web Firebase options configuration and Google Sign-In client ID

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -6,10 +6,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return FirebaseOptions(
+        apiKey: dotenv.env['WEB_API_KEY'] ?? '',
+        appId: dotenv.env['WEB_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['WEB_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['WEB_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['WEB_AUTH_DOMAIN'] ?? '',
+        storageBucket: dotenv.env['WEB_STORAGE_BUCKET'] ?? '',
+        measurementId: dotenv.env['WEB_MEASUREMENT_ID'] ?? '',      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
